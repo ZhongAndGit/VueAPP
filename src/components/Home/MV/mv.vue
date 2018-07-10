8af7d05c1b8c31f567038c7f1b10ac970457b55c
Shannon_沐 <1104551778@qq.com>
2018-07-10 17:55:33 +0800

    更新MV

--- src/components/Home/MV/mv.vue
+++ src/components/Home/MV/mv.vue
@@ -2,19 +2,19 @@
     <div class="mv_c" >
       <Vloading v-show="!showAC"></Vloading>
       <div class="mv" v-for="(item,index) in placeholder"  :id="item.videoId" v-if="item.listV">
-        <div class="title">{{item.title}}</div>
-        <div class="line-msg" v-show="!item.listV['hcVideoUrl']">{{item.listV.message}}</div>
-        <video controls controlsList=" nodownload"  name="media"  v-show="item.listV['hcVideoUrl']">
-          <!--controlsList=" nofullscreen nodownload noremote footbar"-->
-          <source :src="item.listV['hcVideoUrl']" type="video/mp4" >
+        <div class="title" v-text="''?item.Ytitle:item.title"></div>
+        <div class="line-msg" v-show="item.listV.error">{{item.listV.errorMsg}}</div>
+        <video controls controlsList=" nodownload"  name="media" :poster="item.listV.bigHeadImage"  v-show="!item.listV.error" v-if="item.listV.videoUrlModels">
+          <!--controlsList=" nofullscreen nodownload noremote  footbar"-->
+          <source  :src="item.listV.videoUrlModels[0].videoUrl" type="video/mp4" >
         </video>
       </div>
       <div class="mv" v-for="(item,index) in videoData" :id="item.videoId" v-if="item.listV[0]">
         <div class="title">{{item.title}}</div>
-        <div class="line-msg" v-show="!item.listV[0]['hcVideoUrl']">{{item.listV[0].message}}</div>
-        <video controls controlsList=" nodownload"  name="media" >
+        <div class="line-msg" v-show="item.listV.error">{{item.listV.errorMsg}}</div>
+        <video controls controlsList=" nodownload"  name="media" :poster="item.listV[0].bigHeadImage" v-show="!item.listV.error" >
           <!--controlsList=" nofullscreen nodownload noremote footbar"-->
-          <source :src="item.listV[0]['hcVideoUrl']" type="video/mp4" >
+          <source :src="item.listV[0].videoUrlModels[0].videoUrl" type="video/mp4" >
         </video>
       </div>
     </div>
@@ -27,6 +27,7 @@
         videoData:[],
         showAC:false,
         placeholder:[{
+          Ytitle:null,
           title:null,
           listV:null,
           videoId:null,
@@ -53,7 +54,7 @@
       var _this = this;
       this.$jsonp('http://soapi.yinyuetai.com/search/rec').then(function(res){
         var dst = res.placeholder
-        _this.placeholder[0].title = dst.word;
+        _this.placeholder[0].Ytitle = dst.word;
         _this.placeholder[0].videoId = dst.searchUrl.substring(dst.searchUrl.lastIndexOf('/')+1);
         _this.getVideo(dst.searchUrl.substring(dst.searchUrl.lastIndexOf('/')+1))
         res.result.forEach(function(item,index){
@@ -75,11 +76,19 @@
       },
       getVideo(id,index){
         var Pthis = this
-        this.$jsonp('http://www.yinyuetai.com/api/info/get-video-urls?videoId='+id).then(function(res){
+        this.$jsonp('http://ext.yinyuetai.com/main/get-h-mv-info?json=true&videoId='+id).then(function(res){
           if(index != undefined){
-            index.listV.push(res)
+            console.log(res)
+            index.listV.push({
+                  headImage:res.videoInfo.coreVideoInfo.headImage,
+                  bigHeadImage:res.videoInfo.coreVideoInfo.bigHeadImage,
+                  videoUrlModels:res.videoInfo.coreVideoInfo.videoUrlModels,
+                  error:res.videoInfo.coreVideoInfo.error,
+                  errorMsg:res.videoInfo.coreVideoInfo.errorMsg,
+            })
           }else {
-            Pthis.placeholder[0].listV = res;
+            Pthis.placeholder[0].listV = res.videoInfo.coreVideoInfo
+            Pthis.placeholder[0].title = res.videoInfo.coreVideoInfo.videoName
           }
         },function(){
           console.log('失败了呢')
